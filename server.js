fef99a603c64088680dbd6d17cdf5db66f3eeb9d
Mahesh <mjchaudhari@gmail.com>
2016-01-29 08:09:47 +0000

    corrected CORS headers

--- server.js
+++ server.js
@@ -26,7 +26,6 @@ app.use(passport.session()); // persistent login sessions
 
 app.use(function(req, res, next) {
       res.header("Access-Control-Allow-Origin", "*");
-      res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
       res.header('Access-Control-Allow-Methods', '*,OPTIONS,GET,PUT,POST,DELETE');
       res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
       next();
